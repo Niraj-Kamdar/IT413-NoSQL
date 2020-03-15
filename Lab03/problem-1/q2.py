415329f3a815339ec0ed9e1974de3a1f90c6f209
Niraj-Kamdar <51387861+Niraj-Kamdar@users.noreply.github.com>
2020-03-15 21:57:14 +0530

    Update q2.py

--- Lab03/problem-1/q2.py
+++ Lab03/problem-1/q2.py
@@ -12,7 +12,7 @@ def put_item(table_name, content):
 if __name__ == "__main__":
     dynamodb = boto3.resource('dynamodb', region_name='ap-south-1', endpoint_url="http://localhost:8000")
 
-    fd = open(r"data\books.json", "r")
-    books_obj = json.loads(fd.read())
-    obj = books_obj["books"][0]
-    print(put_item("Books", obj))
+    with open(r"data\books.json", "r") as fd:
+        books_obj = json.loads(fd.read())
+        obj = books_obj["books"][0]
+        print(put_item("Books", obj))
